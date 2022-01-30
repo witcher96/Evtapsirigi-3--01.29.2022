c8393b01fb5d1bac125962794f408d9f7b604e7b
witcher96 <hasanaa@code.edu.az>
2022-01-30 18:09:26 +0400

    All done

---
+++ alfasol/Evtapsirigi/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evtapsirigi
+{
+    class Car
+    {
+        public string name;
+        public string model;
+        public string color;
+        public string city;
+        public string New;
+        public string engineType;
+        public double cost;
+
+        public string getinfo()
+        {
+            string output = $"{name}\n " +
+                $"{model}\n" +
+                $" {city}\n " +
+                $"{New}\n" +
+                $" {engineType}\n" +
+                $" {cost} Azn\n" +
+                $" ==============";
+
+            return output;
+
+        }
+    }
+}
---
+++ alfasol/alfa/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace Classcar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+              Console.OutputEncoding = Encoding.Unicode;
+              Console.InputEncoding = Encoding.Unicode;
+
+              Car car1 = new Car();
+              car1.name = "BMW";
+              car1.model = "X5";
+              car1.city = "Baki";
+              car1.engineType = "Diesel";
+              car1.color = "Black";
+              car1.New = "Beli";
+              car1.cost = 15.500;
+
+              Car car2 = new Car();
+              car2.name = "Mercedes";
+              car2.model = "SClass";
+              car2.city = "Lenkeran";
+              car2.engineType = "Gasoline";
+              car2.color = "Red";
+              car2.New = "Xeyr";
+              car2.cost = 25.500;
+
+              Car car3 = new Car();
+              car3.name = "Lamborghini";
+              car3.model = "Sisto elemento";
+              car3.city = "Milan";
+              car3.engineType = "Gasoline";
+              car3.color = "Purple";
+              car3.New = "Beli";
+              car3.cost = 850.500;
+
+
+
+            Car[] cars = new Car[3];
+            cars[0] = car1;
+            cars[1] = car2;
+            cars[2] = car3;
+            
+            foreach(Car item in cars)
+            {
+                item.getinfo();
+            }
+                
+        }
+    }
+}
